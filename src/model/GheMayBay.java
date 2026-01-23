6c997cda0e7679cb5ae1d63afed6507fb750b6c0
ItzAlisa <vuhoanghuyvn@gmail.com>
2026-01-23 23:54:54 +0700

    Code model GheMayBay, HangVe, HeSoGia

---
+++ src/model/GheMayBay.java
@@ -0,0 +1,56 @@
+package model;
+
+public class GheMayBay {
+    private String ma_Ghe;
+    private String ma_May_Bay;
+    private String so_Ghe;
+    private double gia_Ghe;
+
+    public GheMayBay(){}
+
+    public GheMayBay(String ma_Ghe, String ma_May_Bay,String so_Ghe, double gia_Ghe){
+        this.ma_Ghe = ma_Ghe;
+        this.ma_May_Bay = ma_May_Bay;
+        this.so_Ghe = so_Ghe;
+        this.gia_Ghe = gia_Ghe;
+    }
+
+    public GheMayBay(GheMayBay a){
+        this.ma_Ghe = a.ma_Ghe;
+        this.ma_May_Bay = a.ma_May_Bay;
+        this.so_Ghe = a.so_Ghe;
+        this.gia_Ghe = a.gia_Ghe;
+    }
+
+    public String getMa_Ghe() {
+        return ma_Ghe;
+    }
+
+    public void setMa_Ghe(String ma_Ghe) {
+        this.ma_Ghe = ma_Ghe;
+    }
+
+    public String getSo_Ghe() {
+        return so_Ghe;
+    }
+
+    public String getMa_May_Bay() {
+        return ma_May_Bay;
+    }
+
+    public void setMa_May_Bay(String ma_May_Bay) {
+        this.ma_May_Bay = ma_May_Bay;
+    }
+
+    public void setSo_Ghe(String so_Ghe) {
+        this.so_Ghe = so_Ghe;
+    }
+
+    public double getGia_Ghe() {
+        return gia_Ghe;
+    }
+
+    public void setGia_Ghe(double gia_Ghe) {
+        this.gia_Ghe = gia_Ghe;
+    }
+}
---
+++ src/model/HangVe.java
@@ -0,0 +1,45 @@
+package model;
+
+public class HangVe {
+    private String ma_Hang_Ve;
+    private String ten_Hang;
+    private float he_So_Hang_Ve;
+
+    public HangVe(){}
+
+    public HangVe(String ma_Hang_Ve, String ten_Hang, float he_So_Hang_Ve) {
+        this.ma_Hang_Ve = ma_Hang_Ve;
+        this.ten_Hang = ten_Hang;
+        this.he_So_Hang_Ve = he_So_Hang_Ve;
+    }
+
+    public HangVe(HangVe a) {
+        this.ma_Hang_Ve = a.ma_Hang_Ve;
+        this.ten_Hang = a.ten_Hang;
+        this.he_So_Hang_Ve = a.he_So_Hang_Ve;
+    }
+
+    public String getMa_Hang_Ve() {
+        return ma_Hang_Ve;
+    }
+
+    public void setMa_Hang_Ve(String ma_Hang_Ve) {
+        this.ma_Hang_Ve = ma_Hang_Ve;
+    }
+
+    public String getTen_Hang() {
+        return ten_Hang;
+    }
+
+    public void setTen_Hang(String ten_Hang) {
+        this.ten_Hang = ten_Hang;
+    }
+
+    public float getHe_So_Hang_Ve() {
+        return he_So_Hang_Ve;
+    }
+
+    public void setHe_So_Hang_Ve(float he_So_Hang_Ve) {
+        this.he_So_Hang_Ve = he_So_Hang_Ve;
+    }
+}
