9c3bb3452983fbd10554ac5f098a3ddc3f8b4b4b
HoangKhang <nguyenkhang.03012006@gmail.com>
2026-02-09 20:24:42 +0700

    Update DAO

---
+++ src/dal/GiaoDichVeDAO.java
@@ -0,0 +1,133 @@
+package dal;
+import entity.GiaoDichVe;
+import entity.TrangThaiGiaoDich;
+import db.DBConnection;
+
+import javax.swing.*;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.math.BigDecimal;
+
+public class GiaoDichVeDAO {
+    // === insert ===  : lưu 1 một giao dịch vé mới vào database (sql)
+    public boolean insert(GiaoDichVe gd){
+        String sql = """
+            INSERT INTO GiaoDichVe 
+            (maGD, maVeCu, maVeMoi, trangThai, phi, phiChenhLech, lyDoDoi, ngayYeuCau, ngayXuLi)
+            VALUES(?, ?, ?, ?, ?, ?, ?, ? ,?)
+        """;
+        // '?' : placeholder(chỗ trống), mỗi ? tương ứng 1 cột
+
+        try (Connection conn = DBConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)){
+            // ket noi sql
+            ps.setString(1, gd.getMaGD()); // gán chuỗi vào từng dấu ?
+            ps.setString(2, gd.getMaVeCu());
+            ps.setString(3, gd.getMaVeMoi());
+            ps.setInt(4, gd.getTrangThai().ordinal()); // gán enum cho INT
+            ps.setBigDecimal(5, gd.getPhi());
+            ps.setBigDecimal(6, gd.getPhiChenhLech());
+            ps.setString(7, gd.getLyDo());
+
+            if(gd.getNgayYeuCau() != null)
+                ps.setDate(8, Date.valueOf(gd.getNgayYeuCau()));
+            else
+                ps.setNull(8, Types.DATE);
+
+            if(gd.getNgayXuLi() != null)
+                ps.setDate(9, Date.valueOf(gd.getNgayXuLi()));
+            else
+                ps.setNull(9, Types.DATE);
+
+            return ps.executeUpdate() > 0; //--> true
+            // excuteUpdadte: Gửi câu SQL sang SQL Server --> trả về 1 nếu insert thành công 1 dòng, 0 nếu không có dòng nào bị ảnh hường
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    //executeUpdate dung cho INSERT, UPDATE, DELETE
+    //executeQuery dung cho SELECT
+
+    //=== find all ===
+    public List<GiaoDichVe> findAll(){
+        List<GiaoDichVe> list = new ArrayList<>();
+        String sql = "SELECT * FROM GiaoDichVe"; // lấy toàn bộ lịch sử giao dịch
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()){
+            // executeQuery: Gửi SELECT sang SQL Server, SQL Server trả về bảng kết quả, JDBC lưu bảng đó trong ResultSet
+
+            while(rs.next()){
+                // next còn true là còn dòng tiếp theo
+                list.add(mapResultSet(rs));  //mapResultSet(rs) chuyển SQL --> JAVA
+            }
+
+        }catch (SQLException e){
+        e.printStackTrace();
+        }
+        return list;
+    }
+
+    // === find by maVeCu ===
+    public List<GiaoDichVe> findByMaVeCu(String maVeCu){
+        List<GiaoDichVe> list = new ArrayList<>();
+        String sql = "SELECT * FROM GiaoDichVe WHERE maVeCu = ?";
+        try(Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ps.setString(1, maVeCu);
+            ResultSet rs = ps.executeQuery();
+
+            while(rs.next()){
+                list.add(mapResultSet(rs));
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return list;
+    }
+    // ===find by trang thai===
+    public List<GiaoDichVe> findByTrangThai(TrangThaiGiaoDich tt){
+        List<GiaoDichVe> list  = new ArrayList<>();
+        String sql = "SELECT * FROM GiaoDichVe WHERE trangThai = ?";
+        try(Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ps.setInt(1, tt.ordinal());
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()){
+                list.add(mapResultSet(rs));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private GiaoDichVe mapResultSet(ResultSet rs) throws SQLException{
+        // chuyển 1 dòng dữ liệu sql thành 1 object java
+        GiaoDichVe gd = new GiaoDichVe(); // tạo object rỗng
+        gd.setMaGD(rs.getString("maGD"));
+        gd.setMaVeCu(rs.getString("maVeCu"));
+        gd.setMaVeMoi(rs.getString("maVeMoi"));
+        gd.setTrangThai(TrangThaiGiaoDich.fromInt(rs.getInt("trangThai")));
+        gd.setPhi(rs.getBigDecimal("phi"));
+        gd.setPhiChenhLech(rs.getBigDecimal("phiChenhLech"));
+        gd.setLyDo(rs.getString("lyDoDoi"));
+
+        Date ngayYC = rs.getDate("ngayYeuCau");
+        if(ngayYC != null)
+            gd.setNgayYeuCau(ngayYC.toLocalDate());
+
+        Date ngayXL = rs.getDate("ngayXuLy");
+        if(ngayXL != null)
+            gd.setNgayXuLi(ngayXL.toLocalDate());
+
+        return gd;
+    }
+}
--- src/entity/TrangThaiGiaoDich.java
+++ src/entity/TrangThaiGiaoDich.java
@@ -1,7 +1,12 @@
 package entity;
 
 public enum TrangThaiGiaoDich {
-    CHO_XU_LY,
-    DA_XU_LY,
-    TU_CHOI
+    CHO_XU_LY,  //0
+    DA_XU_LY,   //1
+    TU_CHOI;     //2
+
+    public static TrangThaiGiaoDich fromInt(int value){
+        return TrangThaiGiaoDich.values()[value];
+
+    }
 }
